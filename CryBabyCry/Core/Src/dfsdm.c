f058e4c7012d9aa30796d0afc7b5e5caa1cfb28f
Jakub Mandula <jakub@mandula.cz>
2021-05-19 12:01:43 +0200

    add basic RTOS with working printf

--- CryBabyCry/Core/Src/dfsdm.c
+++ CryBabyCry/Core/Src/dfsdm.c
@@ -9,10 +9,10 @@
   * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
   *
   ******************************************************************************
   */
@@ -167,7 +167,7 @@ void HAL_DFSDM_ChannelMspInit(DFSDM_Channel_HandleTypeDef* dfsdm_channelHandle)
     HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
     /* DFSDM1 interrupt Init */
-    HAL_NVIC_SetPriority(DFSDM1_FLT0_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(DFSDM1_FLT0_IRQn, 5, 0);
     HAL_NVIC_EnableIRQ(DFSDM1_FLT0_IRQn);
   /* USER CODE BEGIN DFSDM1_MspInit 1 */
 
--- CryBabyCry/Core/Src/usart.c
+++ CryBabyCry/Core/Src/usart.c
@@ -9,10 +9,10 @@
   * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
   *
   ******************************************************************************
   */
@@ -38,7 +38,7 @@ void MX_USART1_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   husart1.Instance = USART1;
-  husart1.Init.BaudRate = 115200;
+  husart1.Init.BaudRate = 921600;
   husart1.Init.WordLength = USART_WORDLENGTH_8B;
   husart1.Init.StopBits = USART_STOPBITS_1;
   husart1.Init.Parity = USART_PARITY_NONE;
